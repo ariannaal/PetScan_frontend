e333f3d4765d61beaeb2896a44b41f53b664ca23
ariannaal <ariannaloreti25@gmail.com>
2024-10-07 15:46:50 +0200

    fixes blood test info

--- petscan/src/components/BloodTestInfo.jsx
+++ petscan/src/components/BloodTestInfo.jsx
@@ -32,17 +32,17 @@ const BloodTestInfo = () => {
             }
 
             const data = await response.json();
-            console.log('Animali recuperati:', data);
+            console.log('Animali trovati:', data);
             setPets(data);
 
         } catch (error) {
-            console.error('Errore nel recuperare gli animali:', error);
-            setErrorMessage('Errore nel recuperare gli animali');
+            console.error('Errore nel trovare gli animali:', error);
+            setErrorMessage('Errore nel trovare gli animali');
         }
     };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-
 
         const bloodTest = {
             dateOfTest,
@@ -77,71 +77,92 @@ const BloodTestInfo = () => {
 
     return (
         <>
-            <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="dateOfTest">
-                    <Form.Label>Data dell esame</Form.Label>
-                    <Form.Control
-                        type="date"
-                        value={dateOfTest}
-                        onChange={(e) => setDateOfTest(e.target.value)}
-                        required
-                    />
-                </Form.Group>
+            <div className="form-container d-flex flex-column align-items-center justify-content-center">
 
-                <Form.Group controlId="petType" className="mt-3">
-                    <Form.Label>Tipo di animale</Form.Label>
-                    <Form.Control
-                        as="select"
-                        value={petType}
-                        onChange={(e) => setPetType(e.target.value)}
-                        required
-                    >
-                        <option value="">Seleziona il tipo di animale</option>
-                        <option value="CAT">Gatto</option>
-                        <option value="DOG">Cane</option>
-                    </Form.Control>
-                </Form.Group>
+                <h1 className="text-center my-5 login-title">Registra un esame del sangue <span className="underline"></span></h1>
 
-                <Form.Group controlId="testNumber" className="mt-3">
-                    <Form.Label>Numero del test</Form.Label>
-                    <Form.Control
-                        type="number"
-                        value={testNumber}
-                        onChange={(e) => setTestNumber(e.target.value)}
-                        required
-                    />
-                </Form.Group>
+                <Form onSubmit={handleSubmit} className="w-25">
+                    <Form.Group controlId="dateOfTest">
+                        <Form.Label>Data dell&apos;esame</Form.Label>
+                        <Form.Control
+                            type="date"
+                            value={dateOfTest}
+                            onChange={(e) => setDateOfTest(e.target.value)}
+                            required
+                            className="custom-input"
+                        />
+                    </Form.Group>
 
-                <Form.Group controlId="petSelect" className="mt-3">
-                    <Form.Label>Seleziona l animale</Form.Label>
-                    <Form.Control
-                        as="select"
-                        value={selectedPetId}
-                        onChange={(e) => setSelectedPetId(e.target.value)}
-                        required
-                    >
-                        <option value="">Seleziona un animale</option>
-                        {pets
-                            .filter(pet => pet.petType === petType || petType === '')
-                            .map(pet => (
-                                <option key={pet.id} value={pet.id}>
-                                    {pet.name}
-                                </option>
-                            ))}
-                    </Form.Control>
-                </Form.Group>
+                    <Form.Group controlId="petType" className="mt-3">
+                        <Form.Label>Tipo di animale</Form.Label>
+                        <Form.Control
+                            as="select"
+                            value={petType}
+                            onChange={(e) => setPetType(e.target.value)}
+                            required
+                            className="custom-input"
+                        >
+                            <option value="">Seleziona il tipo di animale</option>
+                            <option value="CAT">Gatto</option>
+                            <option value="DOG">Cane</option>
+                        </Form.Control>
+                    </Form.Group>
 
-                {errorMessage && (
-                    <p className="error-message" style={{ color: 'red' }}>
-                        {errorMessage}
-                    </p>
-                )}
+                    <Form.Group controlId="testNumber" className="mt-3">
+                        <Form.Label>Numero dell&apos;esame</Form.Label>
+                        <Form.Control
+                            type="number"
+                            value={testNumber}
+                            onChange={(e) => setTestNumber(e.target.value)}
+                            required
+                            className="custom-input"
+                            min="0"
+                        />
+                    </Form.Group>
 
-                <Button className="mt-4" variant="primary" type="submit">
-                    Invia
-                </Button>
-            </Form>
+                    <Form.Group controlId="petSelect" className="mt-3">
+                        <Form.Label>Seleziona l&apos;animale</Form.Label>
+                        <Form.Control
+                            as="select"
+                            value={selectedPetId}
+                            onChange={(e) => setSelectedPetId(e.target.value)}
+                            required
+                            className="custom-input"
+                            disabled={pets.filter(pet => pet.petType === petType).length === 0} // disabilita il dropdown se non ci sono animali
+                        >
+                            <option value="">Seleziona un animale</option>
+                            {pets
+                                .filter(pet => pet.petType === petType)
+                                .map(pet => (
+                                    <option key={pet.id} value={pet.id}>
+                                        {pet.name}
+                                    </option>
+                                ))
+                            }
+                        </Form.Control>
+
+                        {petType && pets.filter(pet => pet.petType === petType).length === 0 && (
+                            <Form.Label className="mt-2 text-danger">
+                                Nessun {petType === "DOG" ? "cane" : "gatto"} trovato
+                            </Form.Label>
+                        )}
+                    </Form.Group>
+
+                    {errorMessage && (
+                        <p className="error-message">
+                            <span role="img" aria-label="warning">❗️</span> {errorMessage}
+                        </p>
+                    )}
+
+                    <div className="d-flex justify-content-center">
+                        <Button className='button-login mt-5 rounded-pill px-4' type="submit">
+                            Invia
+                        </Button>
+                    </div>
+                </Form>
+            </div>
         </>
+
     );
 };
 

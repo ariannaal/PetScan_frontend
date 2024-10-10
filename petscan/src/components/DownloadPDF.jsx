282528d6bd4a61f721703c0410f05e588402a003
ariannaal <ariannaloreti25@gmail.com>
2024-10-10 11:23:24 +0200

    fixes style for pdf

--- petscan/src/components/DownloadPDF.jsx
+++ petscan/src/components/DownloadPDF.jsx
@@ -1,22 +1,88 @@
 import { jsPDF } from "jspdf";
 import autoTable from 'jspdf-autotable';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import logo from '../assets/images/logo-blue.png';
 
-const DownloadPDF = ({ results }) => {
+const DownloadPDF = ({ results, ownerName, surname, petName, petType, age, gender, breed, dateOfTest, testNumber }) => {
     const downloadPDF = () => {
         const doc = new jsPDF();
 
+        doc.addImage(logo, 'PNG', 10, 5, 25, 25);
+
+
         // titolo
         doc.setFontSize(20);
-        doc.text("Risultati degli esami", 14, 22);
+        doc.setTextColor(0, 0, 0); // colore nero
+        doc.text("Risultati degli esami", 40, 20);
+
+
+        // nome del proprietario
+        if (ownerName) {
+            doc.setFontSize(11);
+            doc.text(`Proprietario: ${ownerName} ${surname}`, 14, 35);
+        }
+
+        //data esame
+        if (dateOfTest) {
+            doc.setFontSize(11);
+            doc.text(`Data dell'esame: ${dateOfTest}`, 140, 35);
+        }
+
+        // nome dell'animale
+        if (petName) {
+            doc.setFontSize(11);
+            doc.text(`Nome dell'animale: ${petName}`, 14, 43);
+        }
+
+        //numero dell'esame
+        if (testNumber) {
+            doc.setFontSize(11);
+            doc.text(`Numero dell'esame: ${testNumber}`, 140, 43);
+        }
+
+        //genere
+        if (gender) {
+            doc.setFontSize(11);
+
+            const genderAnimal = gender === 'FEMALE' ? 'Femmina' : gender === 'MALE' ? 'Maschio' : gender;
+            doc.text(`Genere: ${genderAnimal}`, 14, 51);
+        }
+
+        // specie
+        if (petType) {
+            doc.setFontSize(11);
+
+            const animalType = petType === 'CAT' ? 'Gatto' : petType === 'DOG' ? 'Cane' : petType;
+            doc.text(`Specie: ${animalType}`, 14, 59);
+        }
+
+        // razza
+        if (breed) {
+            doc.setFontSize(11);
+            doc.text(`Razza: ${breed}`, 14, 67);
+        }
+
+        // eta'
+        if (age) {
+            doc.setFontSize(11);
+            doc.text(`Età: ${age}`, 14, 75);
+        }
+
 
         // tabella
         autoTable(doc, {
             head: [['Parametro', 'Valore']],
             body: results.map(result => [
-                { content: result.valueName, styles: { fontStyle: 'bold' } },
-                `${result.value} ${result.unit}`
+                { content: result.valueName || 'N/A', styles: { fontStyle: 'bold' } },
+                `${result.value !== undefined ? result.value : 'N/A'} ${result.unit || ''}`
             ]),
+            startY: 85, // per spostare la tabella in basso
+
+            headStyles: { // stile tabella
+                fillColor: [3, 146, 175],
+                textColor: [255, 255, 255],
+                fontStyle: 'bold',
+            },
         });
 
         // salva esami
@@ -32,6 +98,15 @@ const DownloadPDF = ({ results }) => {
 
 DownloadPDF.propTypes = {
     results: PropTypes.array.isRequired,
+    ownerName: PropTypes.string.isRequired,
+    surname: PropTypes.string.isRequired,
+    petName: PropTypes.string.isRequired,
+    petType: PropTypes.string.isRequired,
+    age: PropTypes.number.isRequired,
+    gender: PropTypes.string.isRequired,
+    breed: PropTypes.string.isRequired,
+    dateOfTest: PropTypes.string.isRequired,
+    testNumber: PropTypes.number.isRequired
 };
 
 export default DownloadPDF;
--- petscan/src/components/Values.jsx
+++ petscan/src/components/Values.jsx
@@ -1,6 +1,6 @@
 
 import { useEffect, useState } from "react";
-import { Col, Row, Spinner } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import DownloadPDF from "./DownloadPDF";
 
@@ -9,13 +9,25 @@ const Values = () => {
     const { bloodTestId } = useParams();
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [results, setResults] = useState([]);
+    const [ownerName, setOwnerName] = useState('');
+    const [surname, setSurname] = useState('');
+    const [testNumber, setTestNumber] = useState('');
+    const [dateOfTest, setDateOfTest] = useState('');
+    const [petName, setPetName] = useState('');
+    const [gender, setGender] = useState('');
+    const [petType, setPetType] = useState('');
+    const [breed, setBreed] = useState('');
+    const [age, setAge] = useState('');
+
+
 
 
     console.log("bloodTestId:", bloodTestId);
 
 
 
-    const [results, setResults] = useState([]);
+
 
     useEffect(() => {
         const fetchResults = async () => {
@@ -34,11 +46,24 @@ const Values = () => {
                 }
 
                 const data = await response.json();
-                console.log("Dati con tutto:", data);
+                console.log("Dati ricevuti:", data);
 
 
-                setResults(data);
-
+                if (data.results && Array.isArray(data.results)) {
+                    setResults(data.results);
+                    setOwnerName(data.ownerName || '');
+                    setSurname(data.surname || '');
+                    setPetType(data.petType || '');
+                    setTestNumber(data.testNumber || '');
+                    setDateOfTest(data.dateOfTest || '');
+                    setPetName(data.petName || '');
+                    setGender(data.gender || '');
+                    setBreed(data.breed || '');
+                    setAge(data.age || '');
+                } else {
+                    console.error('I risultati non sono disponibili o non sono un array:', data);
+                    setResults([]);
+                }
             } catch (error) {
                 console.error('Errore durante il recupero dei risultati:', error);
                 setError(error);
@@ -47,18 +72,18 @@ const Values = () => {
             }
         };
 
-
         if (bloodTestId) {
             fetchResults();
         }
     }, [bloodTestId]);
 
 
+
     return (
         <div className="values-container container" >
             <h1 className="text-center mt-5 mb-5 login-title">Risultati degli esami</h1>
 
-            <DownloadPDF results={results} />
+            <DownloadPDF results={results} ownerName={ownerName} surname={surname} testNumber={testNumber} dateOfTest={dateOfTest} petName={petName} gender={gender} breed={breed} age={age} petType={petType} />
 
             <Row>
 
@@ -69,15 +94,21 @@ const Values = () => {
                     </Row>
 
                     <div className="scrollable-container">
-                        {results && results.map((result, index) => (
-                            <Row key={index} className="align-items-center mb-4">
-                                <Col xs={6} className="text-result">
-                                    <span className="fw-bold">{result.valueName}</span>
-                                    <span className="ms-2">({result.unit})</span>
-                                </Col>
-                                <Col xs={6} className="text-result">{result.value}</Col>
-                            </Row>
-                        ))}
+                        {Array.isArray(results) && results.length > 0 ? (
+                            results.map((result, index) => (
+                                <Row key={index} className="align-items-center mb-4">
+                                    <Col xs={6} className="text-result">
+                                        <span className="fw-bold">{result.valueName || 'N/A'}</span>
+                                        <span className="ms-2">({result.unit ? result.unit.join(', ') : 'N/A'})</span>
+                                    </Col>
+                                    <Col xs={6} className="text-result">{result.value !== undefined ? result.value : 'N/A'}</Col>
+                                </Row>
+                            ))
+                        ) : (
+                            <div>Nessun risultato disponibile.</div>
+                        )}
+
+
                     </div>
                 </Col>
 

87ff46a1e40942ac28d87cf208bb4b3b88e71838
ariannaal <ariannaloreti25@gmail.com>
2024-10-23 14:57:17 +0200

    fixes style

--- petscan/src/components/Account.jsx
+++ petscan/src/components/Account.jsx
@@ -28,11 +28,43 @@ const Account = () => {
             }
         };
 
-        fetchOwner();
+        if (accessToken) {
+            fetchOwner();
+        } else {
+            setOwner(null);
+        }
     }, [accessToken]);
 
-    if (!owner) {
-        return <div>Caricamento...</div>;
+    if (owner === null) {
+        return (
+            <div className="d-flex flex-column mt-5 align-items-center justify-content-center">
+                <h1 className="text-center my-5 login-title test-title">
+                    Il tuo account<span className="underline"></span>
+                </h1>
+                <img
+                    src={pawPrints}
+                    alt="prints"
+                    className="position-absolute  img-fluid paw-prints"
+                    style={{ width: '650px', height: 'auto', zIndex: 0 }}
+                />
+                <div>Effettua il login o registrati per visualizzare il tuo account.</div>
+                <div className="d-flex mt-5 align-items-baseline account-container">
+
+                    <Button className='button-login me-2 rounded-pill px-4' onClick={() => navigate('/auth/login')}>
+                        Login
+                    </Button>
+                    <Button className='button-login me-2 rounded-pill px-4' onClick={() => navigate('/auth/register')}>
+                        Registrati
+                    </Button>
+                </div>
+                <img
+                    src={pawPrints}
+                    alt="prints"
+                    className="position-absolute  img-fluid paws-values"
+                    style={{ width: '650px', height: 'auto', zIndex: 0 }}
+                />
+            </div>
+        );
     }
 
     return (
@@ -49,14 +81,16 @@ const Account = () => {
             />
 
             <div className="account-container">
-
                 <p><strong>Nome:</strong> {owner.name}</p>
                 <p><strong>Cognome:</strong> {owner.surname}</p>
                 <p><strong>Email:</strong> {owner.email}</p>
                 <p><strong>Data di nascita:</strong> {owner.dateOfBirth}</p>
                 <p><strong>Genere:</strong> {owner.gender === 'FEMALE' ? 'Femmina' : 'Maschio'}</p>
                 <div className="d-flex justify-content-center mb-3">
-                    <Button className='button-login mt-4 rounded-pill px-4' onClick={() => navigate('/account/edit')}>Modifica</Button></div>
+                    <Button className='button-login mt-4 rounded-pill px-4' onClick={() => navigate('/account/edit')}>
+                        Modifica
+                    </Button>
+                </div>
             </div>
 
             <img

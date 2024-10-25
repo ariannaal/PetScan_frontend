f9e3f722bfa3035594926f00a57f24fd4f88c830
ariannaal <ariannaloreti25@gmail.com>
2024-10-25 11:57:48 +0200

    fixes style and adds spinner

--- petscan/src/components/Login.jsx
+++ petscan/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import dogImage from '../assets/images/cat-dog.png';
-import { Row, Col, Form, InputGroup, Button } from 'react-bootstrap';
+import { Row, Col, Form, InputGroup, Button, Spinner } from 'react-bootstrap';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -9,6 +9,7 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
+    const [loading, setLoading] = useState(true);
 
     const navigate = useNavigate();
 
@@ -17,6 +18,8 @@ const Login = () => {
             email,
             password,
         };
+
+        setLoading(true);
 
         try {
             const apiUrl = import.meta.env.VITE_API_URL;
@@ -44,6 +47,8 @@ const Login = () => {
         } catch (error) {
             console.error('Errore nel login: ', error);
             setErrorMessage('Credenziali errate');
+        } finally {
+            setLoading(false);
         }
     };
 
@@ -51,51 +56,60 @@ const Login = () => {
         <>
             <div className="login-container">
                 <Row className="h-100">
-                    <Col md={3} lg={5} className="d-flex align-items-center justify-content-center blue-background">
-                    </Col>
-                    <Col md={9} lg={7} className="d-flex justify-content-center white-background">
-                        <div className="d-flex flex-column align-items-center">
-                            <h1 className="login-title">
-                                Login
-                                <span className="underline"></span>
-                            </h1>
-
-                            <div className="w-100 mt-5">
-                                <InputGroup hasValidation className="mb-3">
-                                    <Form.Control
-                                        type="text"
-                                        placeholder="Email"
-                                        required
-                                        className="custom-input"
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                    />
-                                </InputGroup>
-
-                                <InputGroup hasValidation>
-                                    <Form.Control
-                                        type="password"
-                                        placeholder="Password"
-                                        required
-                                        className="custom-input"
-                                        value={password}
-                                        onChange={(e) => setPassword(e.target.value)}
-                                    />
-                                </InputGroup>
-
-                                {errorMessage && (
-                                    <p className="error-message">
-                                        <span role="img" aria-label="warning">❗️</span> {errorMessage}
-                                    </p>
-                                )}
-                            </div>
-                            <Button className='button-login mt-5 rounded-pill px-4' onClick={handleLogin}>Login</Button>
-                            <div className='mt-5'>
-                                Non sei ancora registrato? <Link to="/auth/register" className='signup-link'>Registrati qui</Link>
-                            </div>
-                            <img src={dogImage} alt="puppies" className='dog-picture' />
+                    {loading ? (
+                        <div className="d-flex justify-content-center w-100">
+                            <Spinner animation="border" className="spinner" />
                         </div>
-                    </Col>
+                    ) : (
+                        <>
+                            <Col md={3} lg={5} className="d-flex align-items-center justify-content-center blue-background">
+
+                            </Col>
+                            <Col md={9} lg={7} className="d-flex justify-content-center white-background">
+                                <div className="d-flex flex-column align-items-center">
+                                    <h1 className="login-title">
+                                        Login
+                                        <span className="underline"></span>
+                                    </h1>
+
+                                    <div className="w-100 mt-5">
+                                        <InputGroup hasValidation className="mb-3">
+                                            <Form.Control
+                                                type="text"
+                                                placeholder="Email"
+                                                required
+                                                className="custom-input"
+                                                value={email}
+                                                onChange={(e) => setEmail(e.target.value)}
+                                            />
+                                        </InputGroup>
+
+                                        <InputGroup hasValidation>
+                                            <Form.Control
+                                                type="password"
+                                                placeholder="Password"
+                                                required
+                                                className="custom-input"
+                                                value={password}
+                                                onChange={(e) => setPassword(e.target.value)}
+                                            />
+                                        </InputGroup>
+
+                                        {errorMessage && (
+                                            <p className="error-message">
+                                                <span role="img" aria-label="warning">❗️</span> {errorMessage}
+                                            </p>
+                                        )}
+                                    </div>
+                                    <Button className='button-login mt-5 rounded-pill px-4' onClick={handleLogin}>Login</Button>
+                                    <div className='mt-5'>
+                                        Non sei ancora registrato? <Link to="/auth/register" className='signup-link'>Registrati qui</Link>
+                                    </div>
+                                    <img src={dogImage} alt="puppies" className='dog-picture' />
+                                </div>
+                            </Col>
+                        </>
+                    )}
                 </Row>
             </div>
         </>

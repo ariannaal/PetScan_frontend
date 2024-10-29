ee7bf151296b74ee028cb9777519af5ddbc03106
ariannaal <ariannaloreti25@gmail.com>
2024-10-29 11:48:33 +0100

    fixes style

--- petscan/src/components/Login.jsx
+++ petscan/src/components/Login.jsx
@@ -54,7 +54,7 @@ const Login = () => {
     return (
         <>
             <div className="login-container">
-                <Row className="h-100">
+                <Row className="h-100 height-login">
                     <Col md={3} lg={5} className="d-flex align-items-center justify-content-center blue-background">
                     </Col>
                     <Col md={9} lg={7} className="d-flex justify-content-center white-background">
